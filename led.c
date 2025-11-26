16a99df7282a6ab1faca9e06af5347897515afec
Rhys <121772484+rhysez@users.noreply.github.com>
2025-11-26 22:43:25 +0000

    add comments to main loop

--- led.c
+++ led.c
@@ -5,9 +5,15 @@ int main(void) {
     DDRB = DDRB | (1 << DDB5);
 
     while (1) {
+        // PORTB is now 0010 0000 as result of OR
         PORTB = PORTB | (1 << PORTB5);
+
         _delay_ms(1000);
+
+        // PORTB has AND applied with an inversed mask (1101 1111)
+        // This gives us 0000 0000, resetting PB5 to zero and leaving all other bits unchanged.
         PORTB = PORTB & ~(1 << PORTB5);
+
         _delay_ms(1000);
     }
 }
